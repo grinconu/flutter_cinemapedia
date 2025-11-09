30b7186b5c4cee2a960422da950ac0b01b9e5cf8
German Rincon Urrego <germanurrego@outlook.com>
2025-11-09 14:16:34 -0500

    Add base files

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Placeholder(),
+    );
+  }
+}
