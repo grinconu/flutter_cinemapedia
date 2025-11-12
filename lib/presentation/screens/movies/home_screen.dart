080468d34c2b82384a981435624c6ede2a23ca23
German Rincon Urrego <germanurrego@outlook.com>
2025-11-11 21:11:13 -0500

    Add use of ShellRouter for the bottom bar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,110 +1,18 @@
-import 'package:cinemapedia/config/helpers/human_formats.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = 'home-screen';
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _HomeVIew(),
+      body: childView,
       bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeVIew extends ConsumerStatefulWidget {
-  @override
-  ConsumerState<_HomeVIew> createState() => _HomeVIewState();
-}
-
-class _HomeVIewState extends ConsumerState<_HomeVIew> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).getMoreMovies();
-    ref.read(popularMoviesProvider.notifier).getMoreMovies();
-    ref.read(topRatedMoviesProvider.notifier).getMoreMovies();
-    ref.read(upcomingMoviesProvider.notifier).getMoreMovies();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final isLoading = ref.watch(initialLoadingProvider);
-
-    final nowPlayingSlideMovies = ref.watch(moviesSlideShowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        if (isLoading) const FullScreenLoader(),
-
-        Visibility(
-          visible: !isLoading,
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                floating: true,
-                flexibleSpace: FlexibleSpaceBar(title: CustomAppBar()),
-              ),
-
-              SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return Column(
-                    children: [
-                      MoviesSlidesShow(movies: nowPlayingSlideMovies),
-
-                      MovieHorizontalListView(
-                        movies: nowPlayingMovies,
-                        title: 'Now',
-                        subTitle: HumanFormats.getDay(DateTime.now()),
-                        loadNextPage: ref
-                            .read(nowPlayingMoviesProvider.notifier)
-                            .getMoreMovies,
-                      ),
-
-                      MovieHorizontalListView(
-                        movies: upcomingMovies,
-                        title: 'Upcoming',
-                        loadNextPage: ref
-                            .read(upcomingMoviesProvider.notifier)
-                            .getMoreMovies,
-                      ),
-
-                      MovieHorizontalListView(
-                        movies: popularMovies,
-                        title: 'Popular',
-                        loadNextPage: ref
-                            .read(popularMoviesProvider.notifier)
-                            .getMoreMovies,
-                      ),
-
-                      MovieHorizontalListView(
-                        movies: topRatedMovies,
-                        title: 'The Best',
-                        loadNextPage: ref
-                            .read(topRatedMoviesProvider.notifier)
-                            .getMoreMovies,
-                      ),
-
-                      const SizedBox(height: 15),
-                    ],
-                  );
-                }, childCount: 1),
-              ),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   const CustomBottomNavigation({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (value) => onItemTapped(context, value),
       items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_max),
-          label: 'Home',
-        ),
+        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
 
-BottomNavigationBarItem(
+        BottomNavigationBarItem(
           icon: Icon(Icons.label_outline),
           label: 'Categories',
         ),
